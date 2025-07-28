0f5ba3511a5cb087ec8af2a09f72ebece3220c45
Daniel McQuillen <daniel@mcquilleninteractive.com>
2025-07-28 13:28:20 +1000

    Include django_github_app directly

---
+++ django_github_app/apps.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+from ._typing import override
+
+import logging
+
+logger = logging.getLogger("django_github_app")
+
+
+class GitHubAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_github_app"
+    verbose_name = "GitHub App"
+
+    @override
+    def ready(self):
+        from . import checks  # noqa: F401
+        from .conf import app_settings
+
+        logger.info("GitHub App is ready with settings: %s", app_settings)
+        if app_settings.WEBHOOK_TYPE == "async":
+            from .events import ahandlers  # noqa: F401
+        elif app_settings.WEBHOOK_TYPE == "sync":
+            from .events import handlers  # noqa: F401
